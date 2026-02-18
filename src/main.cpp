e2ace66a72df859efac243cf5011ff7f315ce4ad
Pulkit Kulshreshtha <pulkit.kulshreshtha_cs.aiml24@gla.ac.in>
2026-02-18 15:23:47 +0530

    Add pin definitions for touch sensor and LED

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,6 @@
  * @author Pulkit Kushreshtha
  * @date 18 Feb 2026
  */
+// Pin Definitions
+int touchPin = 7;   ///< Touch sensor connected to digital pin 7
+int ledPin = 13;    ///< LED connected to digital pin 13
